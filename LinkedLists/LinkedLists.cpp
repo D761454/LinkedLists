2141178e125f39574d9ce1ada869529201c65891
D761454 <144056074+D761454@users.noreply.github.com>
2024-05-07 11:18:36 +0100

    Update LinkedLists.cpp

--- LinkedLists/LinkedLists.cpp
+++ LinkedLists/LinkedLists.cpp
@@ -9,7 +9,24 @@ LinkedLists::~LinkedLists() {
 }
 
 ListNode* LinkedLists::MakeNode(ListNode** head, int d) {
+	ListNode* newNode = new ListNode;
+	ListNode* last = *head;
 
+	newNode->data = d;
+	newNode->next = nullptr;
+
+	if (*head == nullptr) {
+		*head = newNode;
+		return newNode;
+	}
+
+	while (last->next != nullptr) {
+		last = last->next;
+	}
+
+	last->next = newNode;
+
+	return newNode;
 }
 
 ListNode* LinkedLists::InsertFirst(ListNode** head, int d) {
