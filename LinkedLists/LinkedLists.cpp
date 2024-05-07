f83281822a7e83a9155dd6c24ed41fd522ec180c
D761454 <144056074+D761454@users.noreply.github.com>
2024-05-07 11:10:08 +0100

    proto

---
+++ LinkedLists/LinkedLists.cpp
@@ -0,0 +1,41 @@
+#include "LinkedLists.h"
+
+LinkedLists::LinkedLists() {
+
+}
+
+LinkedLists::~LinkedLists() {
+
+}
+
+ListNode* LinkedLists::MakeNode(ListNode** head, int d) {
+
+}
+
+ListNode* LinkedLists::InsertFirst(ListNode** head, int d) {
+
+}
+
+void LinkedLists::InsertAfter(ListNode* lastNode, int d) {
+
+}
+
+void LinkedLists::DeleteList(ListNode** node) {
+
+}
+
+void LinkedLists::DeleteAfter(ListNode* node) {
+
+}
+
+ListNode* LinkedLists::GetNode(ListNode* node, int pos) {
+
+}
+
+ListNode* LinkedLists::Find(ListNode* node, int val) {
+
+}
+
+void LinkedLists::PrintList(ListNode* node) {
+
+}
